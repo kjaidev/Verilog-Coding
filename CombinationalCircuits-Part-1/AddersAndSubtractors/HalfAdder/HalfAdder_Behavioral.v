cd287e6b58371a5a655d3bd300081f1e636e8933
kjaidev <jaidev.karthikeyan@gmail.com>
2026-06-05 03:56:42 +0000

    Add HalfAdder: Behavioral

---
+++ CombinationalCircuits-Part-1/AddersAndSubtractors/HalfAdder/HalfAdder_Behavioral.v
@@ -0,0 +1,21 @@
+module HalfAdder_Behavioral (sum, carry, a, b);
+
+	output reg sum, carry;
+	input a, b;
+	
+	always @ (*) begin
+		if (a == 0 & b == 0) begin
+			sum = 0; carry = 0;
+		end
+		else if (a == 0 & b == 1) begin
+			sum = 1; carry = 0;
+		end
+		else if (a == 1 & b == 0) begin
+			sum = 1; carry = 0;
+		end
+		else begin
+			sum = 0; carry = 1;
+		end
+	end
+
+endmodule
