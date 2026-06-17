147dea23b94dbea3e54c06debe42aff933b56d0e
kjaidev <jaidev.karthikeyan@gmail.com>
2026-06-17 03:56:37 +0000

    Add OneXFourDEMUX: Structural

---
+++ CombinationalCircuits-Part-1/Demultiplexers/OneXFourDEMUX/OneXFourDEMUX_Structural.v
@@ -0,0 +1,18 @@
+module OneXFourDEMUX_Structrual (
+
+	output [3:0] y,
+	input [1:0] sel,
+	input d
+);
+
+	wire sel1_not, sel0_not;
+
+	not(sel1_not, sel[1]);
+	not(sel0_not, sel[0]);
+
+	and (y[0], sel1_not, sel0_not, d);
+	and (y[1], sel1_not, sel[0], d);
+	and (y[2], sel[1], sel0_not, d);
+	and (y[3], sel[1], sel[0], d);
+
+endmodule
