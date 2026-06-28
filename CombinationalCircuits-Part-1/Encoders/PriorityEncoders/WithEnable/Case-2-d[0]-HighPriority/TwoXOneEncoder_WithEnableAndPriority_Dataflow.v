2d650e5e92bd372f85040193078741c8624139cc
kjaidev <jaidev.karthikeyan@gmail.com>
2026-06-28 09:05:50 +0000

    Add PriorityEncoder WithEnable Case-2: Dataflow

---
+++ CombinationalCircuits-Part-1/Encoders/PriorityEncoders/WithEnable/Case-2-d[0]-HighPriority/TwoXOneEncoder_WithEnableAndPriority_Dataflow.v
@@ -0,0 +1,12 @@
+module TwoXOneEncoder_WithEnableAndPriority_Dataflow (
+	
+	output y,
+	output v,
+	input [1:0] d,
+	input en
+);
+
+	assign y = en & d[1] & (~d[0]);
+	assign v = en & (d[1] | d[0]);
+
+endmodule
